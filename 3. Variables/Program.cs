e3574245df435de6640f4aaaf15721e7b2bcec8d
nomvula <nzshabangu95@gmail.com>
2023-09-11 09:43:16 +0200

    Add project files.

---
+++ 3. Variables/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name;
+            int age;
+
+            name = "team";
+            age = 19;
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+
+            //freeze console
+            Console.ReadLine();
+        }
+    }
+}
